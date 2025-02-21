9a22bde973e64c1d601a04303194e70e9c9276d4
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-02-22 01:07:12 +0900

    Update Main_BJ13335.java

--- AlgoCS/s0224/Main_BJ13335.java
+++ AlgoCS/s0224/Main_BJ13335.java
@@ -22,26 +22,35 @@ public class Main_BJ13335 {								//트럭
 		int n = Integer.parseInt(st.nextToken());
 		int w = Integer.parseInt(st.nextToken());			//다리 길이
 		int L = Integer.parseInt(st.nextToken());			//최대 하중
-		Queue<Integer> q = new ArrayDeque<>(); 
+		
+		Queue<Integer> truck = new ArrayDeque<>(); 			//트럭 대기열 큐
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
-			q.offer(Integer.parseInt(st.nextToken()));
+			truck.offer(Integer.parseInt(st.nextToken()));	//트럭 대기열 입력
 		}
 		
-		int time = 0;
-		int heavy = 0; 			//현재 다리의 무게
-		int i = 0;				
-
-		
+		Queue<Integer> q = new ArrayDeque<>(); 				//다리 큐
+		for(int i = 0; i < w; i++) {		
+			q.add(0);										//초기 다리는 다리 길이만큼 빈 값으로 가득 채우기 (안 채우고 해보려니 안 되는거 같음)
+		}
+		int time = 0;										//최소 이동 시간
+		int heavy = 0;										//현재 다리의 무게
 		while(!q.isEmpty()) {
 			time++;
-			if(heavy + q.peek() <= L) {				
-				heavy += q.poll();
+			heavy = heavy - q.poll();						//다리에서 나온 애 무게 빼주기
+			if(!truck.isEmpty()) {
+				if(heavy + truck.peek() <= L) {				//다리가 새로 넣을 트럭의 무게를 견딜 수 있다면	
+					heavy = heavy + truck.peek();			//현재 다리 무게 갱신
+					q.offer(truck.poll());					//다리 큐에 트럭 넣기
+				}
+				else {										//그렇지 않으면 빈자리 삽입
+					q.offer(0);
+				}
 			}
-			
-			
 		}
+		
+		System.out.println(time);
 	}
 
 }
