9c6cdf1ea5ba8068128414e9ed5674badc08c143
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-03-14 10:20:32 +0900

    Add files via upload

--- AlgoCS/s0317/Main_BJ1456.java
+++ AlgoCS/s0317/Main_BJ1456.java
@@ -11,37 +11,35 @@ public class Main_BJ1456 {								//거의 소수
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		long a = sc.nextLong();
+		long b = sc.nextLong();
 		
+		int sqrtB = (int) Math.sqrt(b);
 		//에라토스테네스의 체
-		boolean arr[] = new boolean[b+1];				//1부터 b까지 담을 배열
+		boolean arr[] = new boolean[sqrtB+1];				//1부터 b의 제곱근까지 담을 배열 (b까지 갈 이유가 없네)
 		
 		for(int i = 0; i < arr.length; i++) {			
 			arr[i] = true;
 		}
 		
 		arr[0] = arr[1] = false;					
-		for(int i = 2; i <= Math.sqrt(b); i++) {	
+		for(int i = 2; i <= Math.sqrt(sqrtB); i++) {	
 			if(arr[i]) {							
-				for(int j = i * i; j <= b; j += i) {
+				for(int j = i * i; j < arr.length; j += i) {
 					arr[j] = false;
 				}
 			}
 		} // 소수 판별 끝
 		
 		int count = 0;
-		for(int i = 2; i <= b; i++) {
+		for(int i = 2; i < arr.length; i++) {
 			if(arr[i]) {
-				long pow = i*i;
-				while(pow <= b) {
-					if(a <= pow) {
+				long pow = i;
+				while(pow <= (double) b / i) { //오버플로우 방지 -> b와 a를 각각 i로 나눠서 계산
+					if((double) a / i <= pow) {
 						count++;
 					}
-					if(pow > b/i) {
-						break;
-					}
-					
+		
 					pow = pow * i;
 				}
 			}
