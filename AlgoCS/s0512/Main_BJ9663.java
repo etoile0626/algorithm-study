00e7aed654549265b137e022d3a23e209ec7e326
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-05-01 17:54:49 +0900

    Add files via upload

---
+++ AlgoCS/s0512/Main_BJ9663.java
@@ -0,0 +1,10 @@
+package s0512;
+/*
+   * n*n 크기 체스판에서 퀸 n개를 서로 공격하지 못하게 놓기
+   * 놓을 수 있는 방법 수 구하기
+ */
+public class Main_BJ9663 {                                      //N-Queen
+    public static void main(String[] args) {
+
+    }
+}
