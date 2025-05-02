2429dfa7df3784eb2884b05f35821ec4ac2495f1
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-05-02 23:37:15 +0900

    Add files via upload

---
+++ AlgoCS/s0512/Main_BJ1007.java
@@ -0,0 +1,28 @@
+package s0512;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+   * 평면에 n개의 점 존재, 해당 점을 집합 p라고 가정.
+   * p의 벡터 매칭은 벡터의 집합이고, 모든 벡터는 한 점에서 시작하여 또 다른 점에서 끝나는 벡터의 집합.
+   * p에 속하는 점은 모두 한 번씩 쓰여야 함
+   * p의 벡터 매칭에 있는 벡터의 합의 길이의 최솟값 구하기
+   * *절대.상대 오차는 10^-6까지 허용
+ */
+public class Main_BJ1007 {                          //벡터 매칭
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++ ){
+            st = new StringTokenizer()
+            int v1 =
+        }
+    }
+}
