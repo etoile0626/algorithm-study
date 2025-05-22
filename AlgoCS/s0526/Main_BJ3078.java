e101a8ae46211f95dfdeb040583dc8a3c956ca7c
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-05-22 21:05:51 +0900

    Add files via upload

---
+++ AlgoCS/s0526/Main_BJ3078.java
@@ -0,0 +1,22 @@
+package s0526;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    좋은 친구 = 자신과 반 등수 차가 k보다 작거나 같으면서 이름의 길이가 같아야 함
+    n명의 학생들이 성적순으로 주어졌을 때, 좋은 친구가 몇 쌍이나 있는지 구하기
+ */
+public class Main_BJ3078 {                          //좋은 친구
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
