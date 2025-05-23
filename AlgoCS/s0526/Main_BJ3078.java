74af35cd73f27dfa85d3f09d9c3e9d82094061db
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-05-24 02:26:11 +0900

    Add files via upload

--- AlgoCS/s0526/Main_BJ3078.java
+++ AlgoCS/s0526/Main_BJ3078.java
@@ -10,6 +10,7 @@ import java.util.StringTokenizer;
     n명의 학생들이 성적순으로 주어졌을 때, 좋은 친구가 몇 쌍이나 있는지 구하기
  */
 public class Main_BJ3078 {                          //좋은 친구
+    //다시 풀기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,8 +18,25 @@ public class Main_BJ3078 {                          //좋은 친구
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[k];
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = br.readLine().length();
+        }
 
-        
+        int[] cnt = new int[21];            //cnt[i] = 글자의 길이가 i다 / 2~20까지 길이가 가능하므로
+        for(int i = 0; i <= k; i++){
+            cnt[arr[i]]++;
+        }
+
+        long answer = --cnt[arr[0]];         //arr[0]과 착한 친구 수, 본인 제외하기 위해 --연산, 초기값
+
+        for(int i = 1; i < n; i++){
+            if(i + k < n){
+                cnt[arr[i+k]]++;
+            }
+            answer += --cnt[arr[i]];        //arr[i]와 착한 친구 수
+        }
+
+        System.out.println(answer);
     }
 }
