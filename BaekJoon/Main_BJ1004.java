47910c4bbcc5d789fac0c8a1a491c1b675e383ae
etoile0626 <etoile0626@naver.com>
2025-07-05 19:47:52 +0900

    update Main_BJ1004

--- BaekJoon/Main_BJ1004.java
+++ BaekJoon/Main_BJ1004.java
@@ -9,7 +9,15 @@ public class Main_BJ1004 {                          //어린 왕자
         int T = Integer.parseInt(br.readLine());
         int start[] = new int[2];//출발점 입력
         int end[] = new int[2];//도착점 입력
-        int circle[]=new int[3];//원 입력
-        
+        int circle[] = new int[3];//원 입력
+        for(int i=0; i<T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            start[0] = Integer.parseInt(st.nextToken());//출발 x
+            start[1] = Integer.parseInt(st.nextToken());//출발 y
+            end[0] = Integer.parseInt(st.nextToken());//도착 x
+            end[1] = Integer.parseInt(st.nextToken());//도착 y
+            int n = Integer.parseInt(br.readLine());
+            
+        }
     }
 }
